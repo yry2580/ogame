deef1fdc79e1411dfe95db34b9891a7bfda2a0dd
yury <yurangyi@heywoods.cn>
2021-07-28 16:55:08 +0800

    imperium

---
+++ feeling/Html/Imperium/Imperium.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feeling
+{
+    class Imperium
+    {
+        public int Interval = 240; // 分钟
+        public bool AutoLogin = false;
+        public bool Open = false;
+    }
+}
--- feeling/bin/copy2feeling.bat
+++ feeling/bin/copy2feeling.bat
@@ -6,6 +6,7 @@ xcopy "./Release" "./feeling" /y /e
 if exist ".\feeling\GPUCache\" rd /s/q ".\feeling\GPUCache\";
 if exist ".\feeling\Out\" rd /s/q ".\feeling\Out\";
 if exist ".\feeling\OgameServiceLog\" rd /s/q ".\feeling\OgameServiceLog\";
+if exist ".\feeling\UserCfg\" rd /s/q ".\feeling\UserCfg\";
 
 del ".\feeling\UserData.dat" /q;
 del ".\feeling\ex_mission.cfg" /q;
